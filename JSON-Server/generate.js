3c69d05206f4e850397d31a033c16e7276c58fff
Girish Uppal <girish.uppal@gmail.com>
2019-01-17 10:17:56 +1000

    Updated json-server code

---
+++ JSON-Server/generate.js
@@ -0,0 +1,16 @@
+module.exports = function(){
+
+    var faker = require("faker");
+    var _ = require("lodash");
+
+    return {
+
+        people: _.times(100, function(n) { 
+            return {
+                id: n,
+                name: faker.name.findName(),
+                avatar: faker.internet.avatar()
+            }
+        })
+    }
+}
